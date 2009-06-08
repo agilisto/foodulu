cc704019b6c6604f6c9fea56e9ac2d2dadfafb72
Armand du Plessis <armanddp@agilisto.com>
2009-06-09 01:21:07 +0200

    Better form and error styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include 
+  def page_title
+    @page_title || "Foodulu #{controller_name.capitalize} : #{action_name.capitalize}" 
+  end
+  
+  def login_account
+    unless logged_in?
+      link_to 'Login', new_session_path
+    else
+      link_to 'Account', user_path(@current_user)
+    end
+  end
 end
